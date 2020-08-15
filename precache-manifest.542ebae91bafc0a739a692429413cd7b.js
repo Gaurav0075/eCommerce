d7184903b37d19f51efea6426971a21780a688d1
Gaurav <gaurav.kumar0075@gmail.com>
2020-08-16 01:01:57 +0530

    Updates

---
+++ precache-manifest.542ebae91bafc0a739a692429413cd7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8917d522b9dcf936c59f06ef869aa205",
+    "url": "/eCommerce/index.html"
+  },
+  {
+    "revision": "0906ccd0c495c4d05807",
+    "url": "/eCommerce/static/css/main.34df3f6d.chunk.css"
+  },
+  {
+    "revision": "363cc607fc16213fd13d",
+    "url": "/eCommerce/static/js/2.bbff3304.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eCommerce/static/js/2.bbff3304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0906ccd0c495c4d05807",
+    "url": "/eCommerce/static/js/main.5d3fd590.chunk.js"
+  },
+  {
+    "revision": "d64104d226bfa1cad1d1",
+    "url": "/eCommerce/static/js/runtime-main.4820d385.js"
+  }
+]);
